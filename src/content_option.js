41abe29474eb975bbba4a3b16471a965391a007a
satya-subham <satyasubham85@gmail.com>
2023-11-23 12:56:18 +0530

    portfolio

---
+++ src/content_option.js
@@ -0,0 +1,118 @@
+const logotext = "SATYASUBHAM";
+const meta = {
+    title: "Satyasubham",
+    description: "I’m Satyasubham Full stack devloper,currently looking for opportunities to prove myself",
+};
+
+const introdata = {
+    title: "I’m Satyasubham",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop web apps",
+    },
+    description: "As a Full-Stack developer i'm responsible for working on both front-end and back-end development processes. I design, develop, and maintain fully-fledged and functioning platforms with databases or servers. ",
+    your_img_url: "https://avatars.githubusercontent.com/u/105437598?v=4",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "As a Full-Stack developer i'm responsible for working on both front-end and back-end development processes. I design, develop, and maintain fully-fledged and functioning platforms with databases or servers. I love coding and programming. I code cool web applications both fronted and backends. I develop web applications.",
+};
+const worktimeline = [
+    {
+        jobtitle: "Site engineer",
+        where: "Hyderabad",
+        date: "2021",
+    },
+    {
+        jobtitle: "Full stack developer trainee",
+        where: "Digikull bootcamp",
+        date: "2023",
+    },
+];
+
+const skills = [{
+        name: "HTML5",
+        value: 90,
+    },
+    {
+        name: "CSS3",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Node.js",
+        value: 85,
+    },
+    {
+        name: "Express.js",
+        value: 85,
+    },
+    {
+        name: "mongodb",
+        value: 60,
+    },
+];
+
+const services = [{
+        title: "Front-End",
+        description: "Hey there as passonate developer i love to build frontend application using html , css, javascript and react.js.",
+    },
+    {
+        title: "Back-End",
+        description: "also bbuild server side application using node.js, expree.js and mongodb.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://c.ndtvimg.com/2022-01/kkhacht8_mamaearth_625x300_01_January_22.jpg",
+        description: "This mamaearth clone is for get the products and check the products are available in app. user can buy products accroding to his/her neccesity . log in and logbout features also attatched. profile and details page are also available.",
+        link: "https://digikul-mamaearth.vercel.app/",
+    },
+    {
+        img: "https://s3-alpha.figma.com/hub/file/3863763275/43bb9a44-3863-4ff2-ad24-a2de0a50d129-cover.png",
+        description: "This open weather app for everyone to check the weather avability in thier city also can check climat across the world and according to climat back ground also changed. user can also get to know about climate through coordinates.",
+        link: "https://digikull-weather-app.vercel.app/",
+    },
+    {
+        img: "https://www.geckoboard.com/uploads/Inventory-dashboard-example.png",
+        description: "This stock board app shows stock market value with several currencies",
+        link: "https://digikull-react-dashboard.netlify.app/",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "satyasubham85@gmail.com",
+    YOUR_FONE: "+91 7873758206",
+    description: "Hey folks this is my contact info you can react out to me anytime via thse details or also can leave a message to me by filling the contact form. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_dy33wvj",
+    YOUR_TEMPLATE_ID: "template_251q3yo",
+    YOUR_USER_ID: "YlIzEIeLsnlQaqnm3",
+};
+
+const socialprofils = {
+    github: "https://github.com/satya-subham",
+    linkedin: "www.linkedin.com/in/satya-subham-ba0481218",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
